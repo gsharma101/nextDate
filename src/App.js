7e85c1ce7478f0721d655dc9f7370946f0e8c552
unknown <sharmag8121@gmail.com>
2023-06-17 17:32:39 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './app.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
+    const date = new Date();
+    date.setDate(date.getDate() + count);
+    return (
+        <div>
+            <div className="stepsBox">
+                <button onClick={() => setStep(c => c - 1)}>-</button>
+                <div>Steps {step}</div>
+                <button onClick={() => setStep(c => c + 1)}>+</button>
+            </div>
+            <div className="counterBox">
+                <button onClick={() => setCount(c => c - step)}>-</button>
+                <div>Counter {count}</div>
+                <button onClick={() => setCount(c => c + step)}>+</button>
+            </div>
+            <h1>Today is {date.toDateString()}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
