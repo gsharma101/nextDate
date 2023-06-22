37135c6ebe6a8c98b7c56e87c52345e913e57a1e
unknown <sharmag8121@gmail.com>
2023-06-23 02:15:36 +0530

    v2 completed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ function App() {
     const [count, setCount] = useState(0);
     const [step, setStep] = useState(1);
     const date = new Date();
+    function handelReset() {
+        setCount(0);
+        setStep(1);
+    }
     date.setDate(date.getDate() + count);
     return (
         <div>
             <div className="stepsBox">
-                <button onClick={() => setStep(c => c - 1)}>-</button>
-                <div>Steps {step}</div>
-                <button onClick={() => setStep(c => c + 1)}>+</button>
+                <input type='range' min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+                <span>Step: {step}</span>
             </div>
             <div className="counterBox">
                 <button onClick={() => setCount(c => c - step)}>-</button>
-                <div>Counter {count}</div>
+                <div><input style={{ fontSize: "20px" }} type='number' value={count} onChange={e => setCount(Number(e.target.value))} /></div>
                 <button onClick={() => setCount(c => c + step)}>+</button>
             </div>
             <h1>Today is {date.toDateString()}</h1>
+            {(count !== 0 || step !== 1) ? <div>
+                <button onClick={handelReset}>Reset</button>
+            </div> : ""}
         </div>
     )
 }
